a98e12b60b69a4ce489ce21f0c3899e5059764d4
Steven Loria <sloria1@gmail.com>
2016-07-09 21:17:08 -0400

    Add tests

---
+++ tests.py
@@ -0,0 +1,27 @@
+import json
+from pathlib import Path
+
+from schema import Schema, Optional
+
+HERE = Path('.')
+SCHEMA = Schema({
+    'first': str,
+    'last': str,
+    Optional('twitter'): str,
+    Optional('email'): str,
+    Optional('osf'): str,
+    Optional('personal'): str,
+    Optional('github'): str,
+})
+
+def validate(path):
+    source_path = HERE / path
+    for file_path in source_path.glob('*.json'):
+        with file_path.open() as fp:
+            SCHEMA.validate(json.load(fp))
+
+def test_associates():
+    validate('associates')
+
+def test_developers():
+    validate('developers')
